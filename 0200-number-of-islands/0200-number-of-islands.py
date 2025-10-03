3bcb7b191182d8f4ff1c2d56a47045c8e4d09122
minseokim <85022613+algoriminseo@users.noreply.github.com>
2025-10-03 10:19:53 -0400

    Time: 233 ms (84.7%), Space: 20.3 MB (58.01%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -5,25 +5,54 @@
 # 1 <= m, n <= 300
 # grid[i][j] is '0' or '1'.
 
+# DFS version
+# class Solution:
+#     def numIslands(self, grid: List[List[str]]) -> int:
+#         directions = [[1,0], [-1, 0], [0, 1], [0, -1]]
+#         ROWS, COLS = len(grid), len(grid[0])
+#         islands = 0 
+
+#         def dfs(row, col):
+#             if (row < 0 or col < 0 or row >= ROWS or col >= COLS or
+#                 grid[row][col] == "0"):
+#                 return 
+
+#             grid[row][col] = "0"
+#             for dr, dc in directions:
+#                 dfs(row + dr, col + dc)
+
+#         for r in range(ROWS):
+#             for c in range(COLS):
+#                 if grid[r][c] == "1":
+#                     dfs(r, c)
+#                     islands += 1
+        
+#         return islands
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        directions = [[1,0], [-1, 0], [0, 1], [0, -1]]
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
         ROWS, COLS = len(grid), len(grid[0])
         islands = 0 
 
-        def dfs(row, col):
-            if (row < 0 or col < 0 or row >= ROWS or col >= COLS or
-                grid[row][col] == "0"):
-                return 
+        def bfs(r, c):
+            q = deque()
+            grid[r][c] = "0"
+            q.append((r, c))
 
-            grid[row][col] = "0"
-            for dr, dc in directions:
-                dfs(row + dr, col + dc)
+            while q:
+                row, col = q.popleft()
+                for dr, dc in directions:
+                    new_row, new_col = row + dr, col + dc
+                    if new_row < 0 or new_col < 0 or new_row >= ROWS or new_col >= COLS or grid[new_row][new_col] == "0":
+                        continue
+                    q.append((new_row, new_col))
+                    grid[new_row][new_col] = "0"
 
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == "1":
-                    dfs(r, c)
+                    bfs(r, c)
                     islands += 1
-        
         return islands
+
