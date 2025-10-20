b8c88b460df2ef3556080adad96e704fd294fd73
minseokim <85022613+algoriminseo@users.noreply.github.com>
2025-10-20 11:04:48 -0400

    Time: 15 ms (9.88%), Space: 18.5 MB (17.43%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -10,13 +10,13 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         n = len(edges)
-        adj_list = [[] for _ in range(n + 1)]
-        
+        adj_list = [ [] for i in range(n + 1)]
+
         def dfs(node, par):
-            #How to detect cycle with edges?
             if visit[node]:
                 return True
             visit[node] = True
+
             for neighbor in adj_list[node]:
                 if neighbor == par:
                     continue
@@ -24,18 +24,14 @@ class Solution:
                     return True
             return False
 
-        #Limitation : does not know how to connect adj_list with Cycle
         for edge in edges:
             u, v = edge
-            adj_list[u].append(v)
-            adj_list[v].append(u)
+            adj_list[u].append(v) 
+            adj_list[v].append(u)          
             visit = [False] * (n + 1)
-
             if dfs(u, -1):
                 return [u, v]
         return []
-        
-
        
 
 
