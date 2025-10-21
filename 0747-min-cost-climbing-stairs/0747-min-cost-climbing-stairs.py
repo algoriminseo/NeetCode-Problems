6541f579a038a5ba99455a1c50b13a899ec76329
minseokim <85022613+algoriminseo@users.noreply.github.com>
2025-10-21 09:06:04 -0400

    Time: 4 ms (38.64%), Space: 17.8 MB (63.37%) - LeetHub

---
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -0,0 +1,14 @@
+#Top to bottom : Top is last index + 1
+# Constraints:
+
+# 2 <= cost.length <= 1000
+# 0 <= cost[i] <= 999
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        res = [0] * (n + 1)
+
+
+        for i in range(2, n + 1):
+            res[i] = min(res[i - 1] + cost[i - 1], res[i - 2] + cost[i -2])
+        return res[n]
