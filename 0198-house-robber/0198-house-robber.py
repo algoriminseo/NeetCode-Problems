3484c04e3c500d99fa968ccbe0d70adc98a97a95
minseokim <85022613+algoriminseo@users.noreply.github.com>
2025-10-22 10:48:36 -0400

    Time: 0 ms (100%), Space: 17.5 MB (96.69%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -4,20 +4,12 @@
 # 0 <= nums[i] <= 400
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        memo = [-1] * len(nums)
+        rob1, rob2= 0, 0 
 
-        def dfs(i):
-            if i >= len(nums):
-                return 0
-            if memo[i] != -1:
-                return memo[i]
-
-            memo[i] = max(dfs(i + 1), nums[i] + dfs(i + 2))
-            return memo[i]
-        return dfs(0)
-
-
-
-
-
+        for num in nums:
+            temp = max(num + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+        
+        return rob2
             
