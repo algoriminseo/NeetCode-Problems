8ccd717aa35a385235652b77d9d46d25a5ef570e
minseokim <85022613+algoriminseo@users.noreply.github.com>
2025-10-23 08:33:39 -0400

    Time: 0 ms (100%), Space: 17.8 MB (59.03%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -4,16 +4,26 @@
 # 0 <= nums[i] <= 1000
 
 class Solution:
-    
     def rob(self, nums: List[int]) -> int:
-        return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
-   
-    def helper(self, nums): 
-        rob1, rob2 = 0, 0
+        if len(nums) == 1:
+            return nums[0]
+        return max(self.helper(nums[1:]), self.helper(nums[:-1]))
+    
+    def helper(self, nums):
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
 
-        #[rob1, rob2, n, n + 1]
-        for i in range(len(nums)):
-            tmp = max(nums[i] + rob1, rob2)
-            rob1 = rob2 
-            rob2 = tmp
-        return rob2
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+
+        return dp[-1]
+
+
+
+
