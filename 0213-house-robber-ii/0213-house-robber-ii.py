d1403562731899c0864e5a4f448a4bc5b0dae392
minseokim <85022613+algoriminseo@users.noreply.github.com>
2025-10-23 08:43:33 -0400

    Time: 0 ms (100%), Space: 17.9 MB (38.78%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -6,16 +6,11 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
-    
+
     def helper(self, nums):
         rob1, rob2 = 0, 0 
-
         for num in nums:
-            newRob = max(rob1 + num, rob2)
+            temp = max(rob1 + num, rob2)
             rob1 = rob2
-            rob2 = newRob
-        
+            rob2 = temp
         return rob2
-       
-
-
