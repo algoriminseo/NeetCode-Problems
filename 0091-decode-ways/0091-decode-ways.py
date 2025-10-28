2049af280844010ea821e3abf706aeefdc15c4c5
minseokim <85022613+algoriminseo@users.noreply.github.com>
2025-10-28 09:02:22 -0400

    Time: 0 ms (100%), Space: 17.7 MB (89.36%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -4,8 +4,7 @@ class Solution:
 
         for i in range(len(s) -1, -1, -1):
             if s[i] == "0":
-                return 0 
-
+                dp[i] = 0 
             else:
                 dp[i] = dp[i+1]
 
