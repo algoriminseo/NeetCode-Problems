ee3e286df297dd7786df3be84325f1774952345e
minseokim <85022613+algoriminseo@users.noreply.github.com>
2025-11-11 08:46:19 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,5 +1,5 @@
 # Constraints:
-
+# Time Complexity : O(n)
 # 1 <= s.length <= 300
 # 1 <= wordDict.length <= 1000
 # 1 <= wordDict[i].length <= 20
@@ -8,17 +8,19 @@
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+      
         dp = [False] * (len(s) + 1)
         dp[len(s)] = True
 
         for i in range(len(s) -1, -1, -1):
-            for w in wordDict:
-                if (i + len(w) <= len(s) and s[i : i + len(w)] == w):
-                    dp[i] = dp[i + len(w)]
+            for word in wordDict:
+                if i + len(word) < len(s) and s[i : i + len(word)] == word:
+                    dp[i] = dp[i + len(word)]
                 if dp[i]:
                     break
-        
         return dp[0]
+
+        
 
 
 
