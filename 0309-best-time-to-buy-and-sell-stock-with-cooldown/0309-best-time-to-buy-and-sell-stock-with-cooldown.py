dd7326ab172eaad94d09dd28b5f15e187a800d6c
minseokim <85022613+algoriminseo@users.noreply.github.com>
2025-11-13 08:42:25 -0500

    Time: 0 ms (100%), Space: 18.3 MB (67.88%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -0,0 +1,22 @@
+# Constraints:
+
+# 1 <= prices.length <= 5000
+# 0 <= prices[i] <= 1000
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
+        
+        n = len(prices)
+        hold, sold, rest = [0] * n,  [0] * n,  [0] * n
+
+        hold[0], sold[0], rest[0] = -prices[0], 0, 0 
+
+        
+        for i in range(1, n):
+            hold[i] = max(hold[i-1], rest[i-1] - prices[i])
+
+            sold[i] = hold[i-1] + prices[i]
+
+            rest[i] = max(rest[i-1], sold[i-1])
+        return max(rest[n -1], sold[n-1])
