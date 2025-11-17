6f82db847504c10fba448c514e7c2871182872b1
minseokim <85022613+algoriminseo@users.noreply.github.com>
2025-11-17 10:52:56 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -1,6 +1,7 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        if len(s1) + len(s2) != len(s3):
+        # Time Complexity : O(MN)
+        if len(s1) + len(s2) != len(s3): 
             return False
 
         n1, n2 = len(s1), len(s2)
@@ -8,10 +9,12 @@ class Solution:
         dp = [[False] * (n2 + 1) for _ in range(n1 + 1)]
         dp[0][0] = True
 
-        for i in range(n1 + 1):
-            for j in range(n2 + 1):
-                if i > 0 and s1[i -1] == s3[i + j -1] and dp[i-1][j]:  
+
+        for i in range(1, n1 +1):
+            for j in range(1, n2 + 1):
+                if s2[i -1] == s3[i + j -1] and dp[i-1][j]:
                     dp[i][j] = True
-                if j > 0 and s2[j -1] == s3[i+ j -1] and dp[i][j-1]:
+
+                if s1[j - 1] == s3[i + j -1 ] and dp[i][j-1]:
                     dp[i][j] = True
-        return dp[n1][n2]      
+        return dp[n1][n2]
