df5107608b0fb5bfbbaeed685438532bbbfa4574
minseokim <85022613+algoriminseo@users.noreply.github.com>
2025-11-23 10:45:32 -0500

    Time: 7 ms (72.97%), Space: 21.8 MB (5.34%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,19 @@
+# Constraints:
+# 1 <= intervals.length <= 104
+# intervals[i].length == 2
+# 0 <= starti <= endi <= 104
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda pair : pair[0])
+        output = [intervals[0]]
+
+        for start, end in intervals:
+            lastEnd = output[-1][1]
+
+            if start <= lastEnd:
+                output[-1][1] = max(lastEnd, end)
+            else:
+                output.append([start, end])
+        return output 
+
