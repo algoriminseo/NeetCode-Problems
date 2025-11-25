4ab1ca48868026fcfb5dd0cdf1c40ac9f3bbc3a9
minseokim <85022613+algoriminseo@users.noreply.github.com>
2025-11-25 08:43:06 -0500

    Time: 7 ms (32.86%), Space: 19.6 MB (76.9%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,14 @@
+# Constraints:
+
+# 1 <= nums.length <= 3 * 104
+# -3 * 104 <= nums[i] <= 3 * 104
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        xor_result = 0
+        for num in nums:
+            xor_result = xor_result ^ num 
+
+        return xor_result
+
+
+        
