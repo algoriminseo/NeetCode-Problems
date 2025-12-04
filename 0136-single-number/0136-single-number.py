faf7876a45a58de501095ab9e1499fb9b00e4de8
minseokim <85022613+algoriminseo@users.noreply.github.com>
2025-12-04 09:06:41 -0500

    Time: 0 ms (100%), Space: 19.7 MB (22.02%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -4,11 +4,9 @@
 # -3 * 104 <= nums[i] <= 3 * 104
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        xor_result = 0
+        xor_result = 0 
+
         for num in nums:
             xor_result = xor_result ^ num 
-
-        return xor_result
-
-
         
+        return xor_result
