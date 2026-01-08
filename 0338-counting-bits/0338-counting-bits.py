d82941bb8be8bc99640a68819847cdb463b6f73d
minseokim <85022613+algoriminseo@users.noreply.github.com>
2026-01-08 13:38:04 -0500

    Time: 4 ms (66.86%), Space: 20.1 MB (5.78%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,13 +1,11 @@
+# Linear time O(n) and single pass 
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        ans = []
+        dp = [0] * (n + 1)
+        offset = 1
 
-        for i in range(0, n + 1):
-            tmp = 0 
-            while i != 0:
-                if i & 1 == 1:
-                    tmp += 1
-                i = i >> 1
-            ans.append(tmp)
-
-        return ans                
+        for i in range(1, n + 1):
+            if offset * 2 == i:
+                offset = i 
+            dp[i] = 1 + dp[i - offset]
+        return dp 
