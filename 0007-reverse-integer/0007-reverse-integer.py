cb808f92b0bd5a12263d14ad6893e91ef62fcb24
minseokim <85022613+algoriminseo@users.noreply.github.com>
2026-01-12 12:08:01 -0500

    Time: 51 ms (6.47%), Space: 19.3 MB (6.62%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        origin = x
+        x = abs(x)
+        res = int(str(x)[ : : -1])
+
+        if origin < 0:
+            res *= -1
+        
+        if res < -(1 << 31) or res > (1 << 31) -1:
+            return 0
+        return res
+
