44ceb35a1319eb2eef4f40418fe79bed03ab0232
minseokim <85022613+algoriminseo@users.noreply.github.com>
2026-02-02 10:55:59 -0500

    Time: 0 ms (100%), Space: 19.4 MB (32.84%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,7 +1,5 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # 1 <= s.length <= 10^4
-        # s consists of parentheses only '(){}[]'
         stack = []
         hash_map = {")" : "(" , "]": "[", "}" : "{"}
 
@@ -10,8 +8,10 @@ class Solution:
                 if stack and stack[-1] == hash_map[ch]:
                     stack.pop()
                 else:
-                    return False
+                    return False 
             else:
                 stack.append(ch)
 
-        return True if not stack else False
+        return True if not stack else False 
+
+
