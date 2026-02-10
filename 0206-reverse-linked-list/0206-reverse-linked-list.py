4143b08e5edea295e17fa7c2398a3c8ff55fd086
minseokim <85022613+algoriminseo@users.noreply.github.com>
2026-02-10 09:10:07 -0500

    Time: 0 ms (100%), Space: 20.7 MB (18.51%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# 0 <= num of nodes <= 5000
+# -5000 <= Node.val <= 5000
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        #Iterative Sol
+        prev, curr = None, head
+
+        while curr:
+            tmp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = tmp
+
+
+        return prev
+        # Recursive Sol 
+        # if head is None or head.next is None:
+        #     return head
+        
+        # new_head = self.reverseList(head.next)
+
+        # head.next.next = head
+        # head.next = None
+        # return new_head    
