d01f862f19418e932358d29948690e08f96dd2fb
minseokim <85022613+algoriminseo@users.noreply.github.com>
2026-02-22 11:16:09 -0500

    Time: 307 ms (7.21%), Space: 19.9 MB (12.69%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -10,9 +10,7 @@ class Solution:
     # if sorted(sub) == sub , ABB ABB, NOT TRUE!
     # left, right pointers to look for 
     def characterReplacement(self, s: str, k: int) -> int:
-  
         res = 0 
-
         charSet = set(s)
 
         for c in charSet:
@@ -20,10 +18,23 @@ class Solution:
             for r in range(len(s)):
                 if s[r] == c:
                     count += 1
-
+                
                 while (r - l + 1) - count > k:
                     if s[l] == c:
                         count -= 1
                     l += 1
                 res = max(res, r - l + 1)
         return res
+            
+        # for c in charSet:
+        #     count = l = 0 
+        #     for r in range(len(s)):
+        #         if s[r] == c:
+        #             count += 1
+
+        #         while (r - l + 1) - count > k:
+        #             if s[l] == c:
+        #                 count -= 1
+        #             l += 1
+        #         res = max(res, r - l + 1)
+        # return res
