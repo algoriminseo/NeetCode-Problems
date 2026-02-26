28d418f9782a37da9c40ead4dde66e940f9f15d9
minseokim <85022613+algoriminseo@users.noreply.github.com>
2026-02-26 08:49:17 -0500

    Time: 3 ms (59.44%), Space: 47.3 MB (38.91%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -5,17 +5,17 @@ class Solution {
         Hashtable<Integer, Integer> numMap = new Hashtable<>();
     
         for(int i = 0 ; i < nums.length; i++) {
+            int diff = target - nums[i];
+
+            if (numMap.containsKey(diff) ) {
+                return new int[] {i, numMap.get(diff)};
+            }
+
+
             numMap.put(nums[i], i);
         }
 
-        for(int j = 0 ; j < nums.length; j++) {
-            int diff = target-nums[j];
-            if (numMap.containsKey(diff) && (j != numMap.get(diff))) {
-                res[0] = j;
-                res[1] = numMap.get(diff);
-            
-            }
-        }   
-        return res;
+    
+        return new int[] {};
     }
 }
