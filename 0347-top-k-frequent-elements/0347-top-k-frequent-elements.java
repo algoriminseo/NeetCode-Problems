5aaf721477942dd672f17ca8dcb9990befa37bd6
minseokim <85022613+algoriminseo@users.noreply.github.com>
2026-03-02 11:21:15 -0500

    Time: 15 ms (52.75%), Space: 47.9 MB (32.46%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -0,0 +1,28 @@
+// 1 <= nums.length <= 105
+// -104 <= nums[i] <= 104
+// k is in the range [1, the number of unique elements in the array].
+// It is guaranteed that the answer is unique.
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
+
+        for(int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            heap.offer(new int[]{entry.getValue(), entry.getKey()});  
+            if(heap.size() > k) {
+                heap.poll();
+            } 
+        }
+
+        int[] res = new int[k];
+        for(int i = 0; i < k; i++) {
+            res[i] = heap.poll()[1];
+
+        }
+        return res;
+    }
+}
