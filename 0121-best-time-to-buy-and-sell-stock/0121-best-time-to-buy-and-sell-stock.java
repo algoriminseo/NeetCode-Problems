6f5591de3bbe98ddc5f5a3a9a63c669f72352d2c
minseokim <85022613+algoriminseo@users.noreply.github.com>
2026-03-03 08:50:29 -0500

    Time: 2 ms (41.22%), Space: 94.5 MB (50.48%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,19 +1,18 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int left = 0 ;
+        int left = 0;
         int right = left + 1;
-        int res = 0 ;
-        while(right < prices.length) {
-            if (prices[left] < prices[right] ) {
-                res = Math.max(res, prices[right] - prices[left]);
-            } 
+        int res = 0;
+        while (right < prices.length) {
+            if (prices[left] < prices[right]) {
+                res = Math.max(res, prices[right] - prices[left]);    
+            }
             else {
                 left = right;
             }
             right++;
+
         }
         return res;
-
-
     }   
 }
