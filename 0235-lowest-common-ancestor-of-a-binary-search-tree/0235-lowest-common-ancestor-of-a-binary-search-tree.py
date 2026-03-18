9fff71d0ef6b4f2e599f80f1b3ad688bab319608
minseokim <85022613+algoriminseo@users.noreply.github.com>
2026-03-18 11:20:29 -0400

    Time: 72 ms (17.34%), Space: 22.9 MB (34.31%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -20,7 +20,6 @@ class Solution:
         while cur:
             if p.val < cur.val and q.val < cur.val:
                 cur = cur.left
-            
             elif p.val > cur.val and q.val > cur.val:
                 cur = cur.right
             else:
