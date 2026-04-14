6ea7b0680124a2e0c3d34e1f5128b6954b3846b1
minseokim <85022613+algoriminseo@users.noreply.github.com>
2026-04-14 08:31:13 -0400

    Time: 3 ms (74.98%), Space: 22.2 MB (42.48%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -13,15 +13,13 @@ class Solution:
         def dfs(root):
             nonlocal res
             if not root:
-                return 0 
+                return 0
+
             left = dfs(root.left)
             right = dfs(root.right)
-            
             res = max(res, left + right)
 
             return 1 + max(left, right)
-
-
-
-        dfs(root)   
+            
+        dfs(root)
         return res
