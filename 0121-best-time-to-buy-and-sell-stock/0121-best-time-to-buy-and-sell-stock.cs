d3a7b51f8e722e6fba95b26862cff5d2009ea901
minseokim <85022613+algoriminseo@users.noreply.github.com>
2026-04-24 10:23:14 -0400

    Time: 1 ms (99.86%), Space: 56.4 MB (64.85%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int l = 0 , r = 1;
+        int maxP = 0;
+
+
+        while (r < prices.Length) {
+            if (prices[l] < prices[r] ) {
+                int profit = prices[r] - prices[l];
+                maxP = Math.Max(profit, maxP);
+            } else {
+                l = r;
+            }
+            r++;
+        }
+        return maxP;
+    
+    }
+}
